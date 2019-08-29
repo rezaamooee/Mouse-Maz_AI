6006eabd5a63ec76d89bc9c623ab30443c699f35
rezaamooee <sir.rama2000@gmail.com>
2019-08-29 15:36:30 +0430

    Create _Back.cs

---
+++ maz-Step1/_Back.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace maz_Step1
+{
+    class _Back
+    {
+        static public char SetHeadToBack(char HeadDirection)
+        {
+            switch (HeadDirection)
+            {
+                case 'e':
+                    return 'e';
+                case 'w':
+                    return 'w';
+                case 'n':
+                    return 'n';
+                default:
+                    return 's';
+            }
+        }
+        static public int SetMoveToBack(int Row, int Column, char HeadDirection)
+        {
+            if (Row < 12)
+            {
+                Row++;
+                return Row * 13 + Column;
+            }
+            return -1;
+        }
+        static public Boolean CheckMoveToBackIsTrue(int Row, int Column, char[,] Map)
+        {
+            return _PublicFunction.IsRout(++Row, Column,  Map);
+        }
+    }
+}
