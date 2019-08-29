b44b62dcc3dea73b9ea056a2a3f0a0e4adb5a931
rezaamooee <sir.rama2000@gmail.com>
2019-08-29 15:36:32 +0430

    Create _Left.cs

---
+++ maz-Step1/_Left.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace maz_Step1
+{
+    class _Left
+    {
+        static public char SetHeadToLeft(char HeadDirection)
+        {
+            switch (HeadDirection)
+            {
+                case 'e':
+                    return 'n';
+                case 'w':
+                    return 's';
+                case 'n':
+                    return 'w';
+                default:
+                    return 'e';
+            }
+        }
+        static public int SetMoveToLeft(int Row, int Column, char HeadDirection)
+        {
+            if (Column > 0)
+            {
+                Column--;
+                return Row * 13 + Column;
+            }
+            return -1;
+        }
+        static public Boolean CheckMoveLeftIsTrue(int Row, int Column, char[,] Map)
+        {
+            return _PublicFunction.IsRout(Row, --Column, Map);
+        }
+    }
+}
