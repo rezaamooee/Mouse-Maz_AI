8f139f8f2a86679f72c2c7b3829829cb1071b309
rezaamooee <sir.rama2000@gmail.com>
2019-08-29 15:36:45 +0430

    Create _Straight.cs

---
+++ maz-Step1/_Straight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maz_Step1
+{
+    class _Straight
+    {
+        static public char SetHeadToStraight(char HeadDirection)
+        {
+            switch (HeadDirection)
+            {
+                case 'e':
+                    return 'e';
+                case 'w':
+                    return 'w';
+                case 'n':
+                    return 'n';
+                default:
+                    return 's';
+            }
+        }
+        static public int SetMoveToStraight(int Row, int Column, char HeadDirection)
+        {
+            if (Row >0)
+            {
+                Row--;
+                return Row * 13 + Column;
+            }
+            return -1;
+        }
+        static public Boolean CheckMoveStraightIsTrue(int Row, int Column,char[,] Map)
+        {
+            return _PublicFunction.IsRout(--Row, Column, Map);
+        }
+    }
+}
